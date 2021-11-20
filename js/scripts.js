aebcf8b49da064def220a5550e4dbebfcc730de8
armannm <anm4500@gmail.com>
2021-11-20 18:08:02 +0330

    work section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(function () {
+    
+    /* 
+        services
+        ----------------------
+    */    
     new WOW().init();
-})
+    
+    
+    /* 
+        works
+        ----------------------
+    */
+    $('#work').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+
